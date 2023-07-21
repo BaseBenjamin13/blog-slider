d083b6dcc4f27e1ac0c1222fb72ece490abe33cf
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2023-07-21 17:32:06 -0400

    figured out the css - package user most import css file directly like blog-slider/blog-slider.css

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function _nonIterableRest() { throw new TypeError("Invalid attempt to destructur
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i["return"] && (_r = _i["return"](), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; } // import styles from './styles/BlogSlider.module.css';
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; } // import '../blog-slider.css';
 // import 'react-tabs/style/react-tabs.css';
 
 function BlogSlider(props) {
@@ -22,24 +22,24 @@ function BlogSlider(props) {
     _useState2 = _slicedToArray(_useState, 2),
     blogIndex = _useState2[0],
     setBlogIndex = _useState2[1];
-  var _useState3 = (0, _react.useState)(styles.blogContentShowFromRight),
+  var _useState3 = (0, _react.useState)("blogContentShowFromRight"),
     _useState4 = _slicedToArray(_useState3, 2),
     blogClass = _useState4[0],
     setBlogClass = _useState4[1];
   var handleChangeBlogIndex = function handleChangeBlogIndex(num, index) {
     if (num === 1 && blogIndex < props.blogContent[index].blogs.length - 1 || num === -1 && blogIndex >= 1) {
-      if (num === -1) setBlogClass(styles.blogContentHideToRight);
-      if (num === 1) setBlogClass(styles.blogContentHideToLeft);
+      if (num === -1) setBlogClass("blogContentHideToRight");
+      if (num === 1) setBlogClass("blogContentHideToLeft");
       var changeIndex = function changeIndex() {
-        if (num === -1) setBlogClass(styles.blogContentShowFromLeft);
-        if (num === 1) setBlogClass(styles.blogContentShowFromRight);
+        if (num === -1) setBlogClass("blogContentShowFromLeft");
+        if (num === 1) setBlogClass("blogContentShowFromRight");
         setBlogIndex(blogIndex + num);
       };
       setTimeout(changeIndex, 500);
     }
   };
   return props.blogContent && /*#__PURE__*/_react["default"].createElement("div", {
-    className: styles.blogContainer
+    className: "blogContainer"
   }, /*#__PURE__*/_react["default"].createElement("h1", null, "Main Blog"), /*#__PURE__*/_react["default"].createElement(_reactTabs.Tabs, {
     onSelect: function onSelect() {
       return setBlogIndex(0);
@@ -52,102 +52,23 @@ function BlogSlider(props) {
     return /*#__PURE__*/_react["default"].createElement(_reactTabs.TabPanel, {
       key: index
     }, /*#__PURE__*/_react["default"].createElement("div", {
-      className: styles.blogContentContainer
+      className: "blogContentContainer"
     }, /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("p", {
       className: blogClass
     }, /*#__PURE__*/_react["default"].createElement("span", {
-      className: styles.blogTitle
+      className: "blogTitle"
     }, item.blogs[blogIndex].title), item.blogs[blogIndex].content)), /*#__PURE__*/_react["default"].createElement("div", {
-      className: styles.blogNavContainer
+      className: "blogNavContainer"
     }, /*#__PURE__*/_react["default"].createElement("button", {
-      className: styles.blogBackBtn,
+      className: "blogBackBtn",
       onClick: function onClick() {
         return handleChangeBlogIndex(-1, index);
       }
     }, "Back"), /*#__PURE__*/_react["default"].createElement("button", {
-      className: styles.blogNextBtn,
+      className: "blogNextBtn",
       onClick: function onClick() {
         return handleChangeBlogIndex(1, index);
       }
     }, "Next"))));
   })));
 }
-var styles = {
-  blogContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    // backgroundColor: var(--myGray),
-    width: '80vw',
-    borderRadius: '50px',
-    margin: '0 auto',
-    marginTop: '1rem',
-    padding: '1rem'
-    // color: var(--myDarkGray),
-  },
-
-  blogContentContainer: {
-    // text-align: center;
-  },
-  blogTitle: {
-    // font-size: 200%;
-    // margin: 5px;
-  },
-  // blogContentShowFromRight, blogContentShowFromLeft, blogContentHideToRight, blogContentHideToLeft: {
-  // font-size: 120%;
-  // margin: 1rem;
-  // },
-
-  blogContentShowFromRight: {
-    // animation: showFromRight 500ms 1;
-  },
-  blogContentShowFromLeft: {
-    // animation: showFromLeft 500ms 1;
-  },
-  blogContentHideToRight: {
-    // animation: hideToRight 600ms 1;
-  },
-  blogContentHideToLeft: {
-    // animation: hideToLeft 600ms 1;
-  }
-
-  // blogBackBtn, blogNextBtn: {
-  // margin: 5px;
-  // margin-left: 10px;
-  // margin-right: 10px;
-  // font-size: 140%;
-  // },
-
-  // @keyframes showFromRight {
-  //     0% {
-  //         transform: translateX(110%);
-  //     }
-  //     100% {
-  //         transform: translateX(0%);
-  //     }
-  // }
-  // @keyframes showFromLeft {
-  //     0% {
-  //         transform: translateX(-110%);
-  //     }
-  //     100% {
-  //         transform: translateX(0%);
-  //     }
-  // }
-  // @keyframes hideToLeft {
-  //     0% {
-  //         transform: translateX(0%);
-  //     }
-  //     100% {
-  //         transform: translateX(-110%);
-  //     }
-  // }
-  // @keyframes hideToRight {
-  //     0% {
-  //         transform: translateX(0%);
-  //     }
-  //     100% {
-  //         transform: translateX(110%);
-  //     }
-  // }
-};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
-// import styles from './styles/BlogSlider.module.css';
+// import '../blog-slider.css';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 // import 'react-tabs/style/react-tabs.css';
 
 export default function BlogSlider(props) {
 
     const [blogIndex, setBlogIndex] = useState(0)
-    const [blogClass, setBlogClass] = useState(styles.blogContentShowFromRight)
+    const [blogClass, setBlogClass] = useState("blogContentShowFromRight")
 
+  
 
     const handleChangeBlogIndex = (num, index) => {
         if (num === 1 && blogIndex < props.blogContent[index].blogs.length - 1 || num === -1 && blogIndex >= 1) {
-            if (num === -1) setBlogClass(styles.blogContentHideToRight)
-            if (num === 1) setBlogClass(styles.blogContentHideToLeft)
+            if (num === -1) setBlogClass("blogContentHideToRight")
+            if (num === 1) setBlogClass("blogContentHideToLeft")
             const changeIndex = () => {
-                if (num === -1) setBlogClass(styles.blogContentShowFromLeft)
-                if (num === 1) setBlogClass(styles.blogContentShowFromRight)
+                if (num === -1) setBlogClass("blogContentShowFromLeft")
+                if (num === 1) setBlogClass("blogContentShowFromRight")
                 setBlogIndex(blogIndex + num);
             }
             setTimeout(changeIndex, 500)
@@ -24,7 +25,8 @@ export default function BlogSlider(props) {
 
     return (
         props.blogContent &&
-        <div className={styles.blogContainer}>
+        <div className="blogContainer">
+        {/* <div className={styles.blogContainer}> */}
             <h1>Main Blog</h1>
             <Tabs onSelect={() => setBlogIndex(0)}>
                 <TabList>
@@ -41,16 +43,21 @@ export default function BlogSlider(props) {
                     props.blogContent.map((item, index) => {
                         return (
                             <TabPanel key={index}>
-                                <div className={styles.blogContentContainer}>
+                                <div className="blogContentContainer">
+                                {/* <div className={styles.blogContentContainer}> */}
                                     <div>
                                         <p className={blogClass}>
-                                            <span className={styles.blogTitle}>{item.blogs[blogIndex].title}</span>
+                                            <span className="blogTitle">{item.blogs[blogIndex].title}</span>
+                                            {/* // <span className={styles.blogTitle}>{item.blogs[blogIndex].title}</span> */}
                                             {item.blogs[blogIndex].content}
                                         </p>
                                     </div>
-                                    <div className={styles.blogNavContainer}>
-                                        <button className={styles.blogBackBtn} onClick={() => handleChangeBlogIndex(-1, index)}>Back</button>
-                                        <button className={styles.blogNextBtn} onClick={() => handleChangeBlogIndex(1, index)}>Next</button>
+                                    <div className="blogNavContainer">
+                                    {/* <div className={styles.blogNavContainer}> */}
+                                        <button className="blogBackBtn" onClick={() => handleChangeBlogIndex(-1, index)}>Back</button>
+                                        <button className="blogNextBtn" onClick={() => handleChangeBlogIndex(1, index)}>Next</button>
+                                        {/* <button className={styles.blogBackBtn} onClick={() => handleChangeBlogIndex(-1, index)}>Back</button>
+                                        <button className={styles.blogNextBtn} onClick={() => handleChangeBlogIndex(1, index)}>Next</button> */}
                                     </div>
                                 </div>
                             </TabPanel>
@@ -60,89 +67,4 @@ export default function BlogSlider(props) {
             </Tabs>
         </div>
     )
-
-}
-
-const styles = {
-    blogContainer: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        // backgroundColor: var(--myGray),
-        width: '80vw',
-        borderRadius: '50px',
-        margin: '0 auto',
-        marginTop: '1rem',
-        padding: '1rem',
-        // color: var(--myDarkGray),
-    },
-    
-    blogContentContainer: {
-        // text-align: center;
-    },
-    
-    blogTitle: {
-        // font-size: 200%;
-        // margin: 5px;
-    },
-    
-    // blogContentShowFromRight, blogContentShowFromLeft, blogContentHideToRight, blogContentHideToLeft: {
-        // font-size: 120%;
-        // margin: 1rem;
-    // },
-    
-    blogContentShowFromRight: {
-        // animation: showFromRight 500ms 1;
-    },
-    blogContentShowFromLeft: {
-        // animation: showFromLeft 500ms 1;
-    },
-    blogContentHideToRight: {
-        // animation: hideToRight 600ms 1;
-    },
-    blogContentHideToLeft: {
-        // animation: hideToLeft 600ms 1;
-    },
-    
-    // blogBackBtn, blogNextBtn: {
-        // margin: 5px;
-        // margin-left: 10px;
-        // margin-right: 10px;
-        // font-size: 140%;
-    // },
-    
-    
-    // @keyframes showFromRight {
-    //     0% {
-    //         transform: translateX(110%);
-    //     }
-    //     100% {
-    //         transform: translateX(0%);
-    //     }
-    // }
-    // @keyframes showFromLeft {
-    //     0% {
-    //         transform: translateX(-110%);
-    //     }
-    //     100% {
-    //         transform: translateX(0%);
-    //     }
-    // }
-    // @keyframes hideToLeft {
-    //     0% {
-    //         transform: translateX(0%);
-    //     }
-    //     100% {
-    //         transform: translateX(-110%);
-    //     }
-    // }
-    // @keyframes hideToRight {
-    //     0% {
-    //         transform: translateX(0%);
-    //     }
-    //     100% {
-    //         transform: translateX(110%);
-    //     }
-    // }
-    
 }
